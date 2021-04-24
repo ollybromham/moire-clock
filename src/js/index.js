b5bc9158ea18ee8bea2060a23e96b3ea0533a155
Olly Bromham <ollybromham@gmail.com>
2021-04-24 13:16:27 +0100

    upgrade to use svgs

--- src/js/index.js
+++ src/js/index.js
@@ -7,29 +7,14 @@ function getTime(noTransition){
   let m = d.getMinutes();
 
   if (!noTransition) {
-    if (h === 0) {
-      hour.classList.remove('transition')
-    } else {
-      hour.classList.add('transition')
-    }
-
-    if (m === 0) {
-      minute.classList.remove('transition')
-    } else {
-      minute.classList.add('transition')
-    }
+    hour.classList.add('transition')
   }
 
   hour.style.transform = 'rotate(' + (h * 30) + 'deg)';
   minute.style.transform = 'rotate(' + (m * 6) + 'deg)';
 }
 
-window.addEventListener('DOMContentLoaded', (event) => {
-  for(i=0;i<275;i++){
-    hour.appendChild(document.createElement('li'))
-    minute.appendChild(document.createElement('li'))
-  }
-
+window.addEventListener('DOMContentLoaded', () => {
   getTime(true);
-  setInterval(getTime, 100);
+  setInterval(getTime, 5000);
 });
